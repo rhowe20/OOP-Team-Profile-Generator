2d92530e841cc80d9531e2ee483e482dd7d7894d
Rebecca Howe <Rebeccahowe20@gmail.com>
2021-01-29 17:13:32 -0800

    prompt questions

--- index.js
+++ index.js
@@ -3,173 +3,114 @@ const fs = require('fs');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+const Employee = require ('./lib/employee');
 
-managerPrompt();
 
-function managerPrompt(){
-inquirer
-.prompt([
+const handleAnswers = [];
+
+const promptQs = [
   {
-    type: 'input',
-    message: 'Welcome! What is your team manager\'s name?',
-    name: 'managerName'
-  },{
-    type: 'input',
-    message: 'What is the manager\'s ID number?',
-    name: 'managerID'
-  },{
-    type: 'input',
-    message: 'What is the manager\'s email address?',
-    name: 'managerEmail'
-  },{
-    type: 'input',
-    message: 'What is the manager\'s office number?',
-    name: 'managerOffice'
-  },{
     type: 'list',
-    message: 'Would you like to add more members to your team?',
-    name: 'newEmployee',
+    message: 'Welcome! Thank you for your using our app. Who is the employee you would like to add?',
+    name: 'role',
     choices: [
-        'Engineer',
-        'Intern',
-        'I am done making my team!'
-    ]
-  }
-])
-  .then(response => {
-  const manager = new Manager(response.managerName, response.managerID, response.managerEmail, response.managerOffice);
-
-  const managerAnswers = [];
-
-  managerAnswers.push(manager);
-  console.log(managerAnswers)
-  if(response.newEmployee === 'Engineer'){
-    engineerPrompts();
-  }else if(response.newEmployee === 'Intern'){
-    internPrompts();
-  }else{
-    console.log('Thank you! Your team is all set up!')
-  }
-})};
-
-
-
-function engineerPrompts(){
-
-  inquirer
-  .prompt([
-  {
-    type: 'input',
-    message: 'What is the engineer\'s name?',
-    name: 'engineerName'
-  },{
-    type: 'input',
-    message: 'What is the engineer\'s ID number?',
-    name: 'engineerID'
-  },{
-    type: 'input',
-    message: 'What is the engineer\'s Github account name?',
-    name: 'engineerGithub'
-  },{
-    type: 'list',
-    message: 'Thank you! What would you like to do next?',
-    name: 'newEmployee',
-    choices: [
-      'Intern',
+      'Manager',
       'Engineer',
-      'I am done making my team!'
-    ]
-  }
-])
-  .then(response => {
-  const engineer = new Engineer(response.engineerName, response.engineerID, response.engineerGithub);
-
-  const engineerAnswers = [];
-
-  engineerAnswers.push(engineer);
-  console.log(engineerAnswers)
-  if(response.newEmployee === 'Engineer'){
-    engineerPrompts();
-  }else if(response.newEmployee === 'Intern'){
-    internPrompts();
-  }else{
-    console.log('Thank you! Your team is all set up!')
-  }
-})};
-
-
-
-function internPrompts(){
-
-  inquirer
-  .prompt([
-  {
-    type: 'input',
-    message: 'What is the Intern\'s name?',
-    name: 'internName'
-  },{
-    type: 'input',
-    message: 'What is the Intern\'s ID number?',
-    name: 'internID'
-  },{
-    type: 'input',
-    message: 'What is the Intern\'s school name?',
-    name: 'internSchool'
-  },{
-    type: 'list',
-    message: 'Thank you! What would you like to do next?',
-    name: 'newEmployee',
-    choices: [
       'Intern',
-      'Engineer',
-      'I am done making my team!'
     ]
-  }
-])
-.then(response => {
-  const intern = new Intern(response.internName, response.internID, response.internschool);
+  },{
+    type: 'input',
+    message: `What is the employee name?`,
+    name: 'name',
+  },{
+    type: 'input',
+    message: `What is the employee ID number?`,
+    name: 'id'
+  },{
+    type: 'input',
+    message: `What is the employee email?`,
+    name: 'email'
+  }]
 
-  const internAnswers = [];
+  
+function createEmployee(){
+  inquirer.prompt(promptQs)
+  .then(function ({name, id, email, role}) {
+    let extra = '';
+    if (role === 'Manager') {
+        extra = 'office number';
+    } else if (role === 'Engineer') {
+        extra = 'Github username';
+    } else {
+        extra = 'school';
+    }
 
-  internAnswers.push(intern);
-  console.log(internAnswers)
-  if(response.newEmployee === 'Engineer'){
-    engineerPrompts();
-  }else if(response.newEmployee === 'Intern'){
-    internPrompts();
-  }else{
-    console.log('Thank you! Your team is all set up!')
-  }
+  inquirer.prompt([{
+    message: `What is the employee's ${extra}?`,
+    name: 'extra',
+    },{
+    message: 'Would you like to add another employee?',
+    name: 'newEmployee',
+    type: 'confirm',
+    }])
+
+  .then(function({extra, newEmployee}) {
+      let emp;
+      if (role === 'Manager') {
+          emp = new Manager(name, id, email, extra);
+      } else if (role === 'Engineer') {
+          emp = new Engineer(name, id, email, extra);
+      } else {
+          emp = new Intern(name, id, email, extra);
+      }
+      handleAnswers.push(emp);
+      newEmpCards(emp)
+
+  .then(function() {
+      if (newEmployee === true) {
+          createEmployee();
+      } else {
+          // endHtml();
+      }
+  });
+  })
 })};
 
-// 1. predefine the 3 classes/constructor functions for the three types of employees : manager, engineer, intern
-// 2. get info from the client about each employee to be added
-//    2.1 ask for name and other info of the manager
-//    2.2 ask if client wants to add another employee or exit
-//    3.3 repeat  
-//    2.2 until exit;
-// greet and ask for managers name: 
-// build an employee of type manager using a constructor (by passing all the info into the constructor)
-    // generate html with info of the employee (by running build command)
-// ask client if they want to add more interns or enginners or exit 
-// if they want to exit, finish the app
-// if the want to build a new employer, add one to html using the constructor of the apropriate type 
+// create html
 
 
 
 
-// function Employee(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.buildHtml = function buildHtml() {
-//       console.log(this.name);
-//       console.log(this.id);
-//       console.log(this.role);
-//     }
-//   }
-//   function Manager(name, id, role) {
-//     this.role = role;
-//     Employee.call(this, name, id);
-//   }
-//   const manager = new Manager('A', 'B', 'C');
-//   manager.buildHtml();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createHtml(handleAnswers){
+//   fs.writeFile('employee.html', handleAnswers, (err) => { 
+      
+//     if (err) throw err; 
+// }) 
+// }
+
+
